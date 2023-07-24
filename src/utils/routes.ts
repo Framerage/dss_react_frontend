c4d27dd48f0d9782ffd07cc43163b562455d18e7
framerage <officialigonin@mail.ru>
2023-07-24 18:15:22 +0400

    edit main url

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -12,7 +12,7 @@ export const FOR_GH_PAGES = "";
 // export const FOR_GH_PAGES = "/dss_react_frontend";
 
 export const APP_GENERAL_ROUTES: RoutesType = {
-  main: {link: "/", component: MainPage, index: true},
+  main: {link: FOR_GH_PAGES, component: MainPage, index: true},
   catalog: {link: FOR_GH_PAGES + "/cards", component: Catalog},
   catalogCard: {link: FOR_GH_PAGES + "/cards/:id", component: CatalogCard},
   contacts: {link: FOR_GH_PAGES + "/contacts", component: null},
@@ -24,7 +24,7 @@ export const APP_GENERAL_ROUTES: RoutesType = {
   error: {link: "*", component: LoginPage},
 };
 export const APP_AUTH_ROUTES: RoutesType = {
-  main: {link: "/", component: MainPage, index: true},
+  main: {link: FOR_GH_PAGES, component: MainPage, index: true},
   catalog: {link: FOR_GH_PAGES + "/cards", component: Catalog},
   catalogCard: {link: FOR_GH_PAGES + "/cards/:id", component: CatalogCard},
   contacts: {link: FOR_GH_PAGES + "/contacts", component: null},
