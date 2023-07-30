a3798baf1034b55f3dba1df8a376ebb3c7c391a1
Artem Igonin <officialigonin@mail.ru>
2023-07-30 04:35:32 +0400

    create-card adaptive

--- src/pages/creatingCard/CreatingCard.tsx
+++ src/pages/creatingCard/CreatingCard.tsx
@@ -25,6 +25,8 @@ const CreatingCard = () => {
   const creatingStatus =
     creatingResult && creatingResult.success ? creatingResult.success : false;
   const [fullDescripValue, setFullDescripValue] = useState("");
+  const [choosedFileIndex, setChoosedFileIndex] = useState(0);
+  const [fileSizeError, setFileSizeError] = useState(false);
   const [cardImagesUrls, setCardImagesUrls] = useState<
     {fileBody: string; fileName: string; id: number}[]
   >([]);
@@ -49,11 +51,15 @@ const CreatingCard = () => {
     dispatch(createNewCatalogCardFx({card: card, auth: authUser.token || ""}));
   };
   const createImgString = async (fileList: FileList | null) => {
+    setFileSizeError(false);
     if (!fileList) {
       return;
     }
     const body = fileList[0];
-    console.log(body, "body");
+    if (body.size > 1000000) {
+      setFileSizeError(true);
+      return;
+    }
     const imgResult = await generateFileData({
       fileBody: body,
       fileName: body.name,
@@ -69,13 +75,18 @@ const CreatingCard = () => {
     ]);
   };
 
-  const onRemoveFile = (id: number) =>
+  const onRemoveFile = (id: number) => {
     setCardImagesUrls(() => {
       const newFiles = cardImagesUrls.filter(file => file.id !== id);
       return newFiles.map((file, index) => {
         return {...file, id: index};
       });
     });
+    if (choosedFileIndex) {
+      setChoosedFileIndex(choosedFileIndex - 1);
+    }
+  };
+  const onChoosedFile = (id: number) => setChoosedFileIndex(id);
 
   useEffect(() => {
     if (!creatingResult) {
@@ -103,34 +114,37 @@ const CreatingCard = () => {
     <div className={classes.cardCreatingContainer}>
       <div className={classes.firstSection}>
         <div className={classes.firstSectPreview}>
-          <div className={classes.imageWindow}>
-            {cardImagesUrls && cardImagesUrls.length > 0 && (
-              <img
-                src={setBase64Image("", cardImagesUrls[0].fileBody)}
-                alt="cardImg"
-                className={classes.previewImg}
-              />
-            )}
-          </div>
-
           <div className={classes.previewAddBlock}>
-            <label htmlFor="addImgUrl" className={classes.previewAddFile}>
-              + Add image
-            </label>
-            <input
-              type="file"
-              name="imgUrl"
-              id="addImgUrl"
-              onChange={e => {
-                createImgString(e.target.files);
-              }}
-              style={{display: "none"}}
-            />
+            <div className={classes.addBtns}>
+              <label htmlFor="addImgUrl" className={classes.previewAddFile}>
+                + Add image
+              </label>
+              <input
+                type="file"
+                name="imgUrl"
+                id="addImgUrl"
+                onChange={e => {
+                  createImgString(e.target.files);
+                }}
+                style={{display: "none"}}
+              />
+              {fileSizeError && (
+                <span className={classes.errAddedText}>
+                  File more than 10mb
+                </span>
+              )}
+            </div>
+
             {cardImagesUrls.length > 0 &&
               cardImagesUrls.map((image, index) => (
                 <div key={index} className={classes.previewFilesList}>
-                  <span className={classes.previewFileName}>
-                    {index + 1}.&nbsp;{image.fileName ? image.fileName : "-"}{" "}
+                  <span
+                    className={cn(classes.previewFileName, {
+                      [classes.choosedFile]: choosedFileIndex === index,
+                    })}
+                    onClick={() => onChoosedFile(index)}
+                  >
+                    {index + 1}.&nbsp;{image.fileName ? image.fileName : "-"}
                   </span>
                   <span
                     onClick={() => onRemoveFile(index)}
@@ -140,6 +154,18 @@ const CreatingCard = () => {
                   </span>
                 </div>
               ))}
+          </div>
+          <div className={classes.imageWindow}>
+            {cardImagesUrls && cardImagesUrls.length > 0 && (
+              <img
+                src={setBase64Image(
+                  "",
+                  cardImagesUrls[choosedFileIndex].fileBody,
+                )}
+                alt="cardImg"
+                className={classes.previewImg}
+              />
+            )}
           </div>
         </div>
         <div className={classes.firstSectBotmBlock}>
