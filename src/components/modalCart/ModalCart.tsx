5fa4bc1da1a9d337258ad7d2f648e8ffe2a6465c
Artem Igonin <officialigonin@mail.ru>
2023-08-06 02:35:39 +0400

    edit modal cart

--- src/components/modalCart/ModalCart.tsx
+++ src/components/modalCart/ModalCart.tsx
@@ -9,6 +9,8 @@ import {isShoppingCartUse} from "store/modules/cart/actions";
 import RemoveIcon from "assets/icons/btn-remove.svg";
 import EmptyCartImg from "assets/icons/emptyIcon.svg";
 import ArrowIcon from "assets/icons/arrow.svg";
+import {Link} from "react-router-dom";
+import {APP_AUTH_ROUTES} from "utils/routes";
 const ModalCart = () => {
   const dispatch = useDispatch<AppDispatch>();
   const isCartOpened = useSelector(isShopCartUse);
@@ -17,16 +19,37 @@ const ModalCart = () => {
     console.log("close cart");
     dispatch(isShoppingCartUse(false));
   };
-  // const items = [
-  //   {imageURL: "", title: "---", price: 111},
-  //   {imageURL: "", title: "---", price: 111},
-  //   {imageURL: "", title: "---", price: 111},
-  //   {imageURL: "", title: "---", price: 111},
-  //   {imageURL: "", title: "---", price: 111},
-  //   {imageURL: "", title: "---", price: 111},
-  //   {imageURL: "", title: "---", price: 111},
-  // ];
-  const items: any[] = [];
+  // const test = {
+  //   createdAt: "2023-07-28T21:32:26.136Z",
+  //   descrip: "Simple and nice decise for your home",
+  //   fullDescrip:
+  //     "Its does not afraid water and and sun, except for high temperature",
+  //   imgUrl: [""],
+  //   likes: 0,
+  //   price: 1599,
+  //   theme: "volPrinter",
+  //   title: "Decor vases",
+  //   updatedAt: "2023-07-28T21:32:26.136Z",
+  //   user: {
+  //     bonuses: 0,
+  //     _id: "64c21f5a3dc3aa01c651e175",
+  //     name: "ArtemAdm",
+  //     email: "officialigonin@mail.ru",
+  //   },
+  //   viewsCount: 0,
+  //   __v: 0,
+  //   _id: "64c433ea4ffff67da2b905be",
+  // };
+  const items = [
+    {imageURL: "", title: "---", price: 111},
+    {imageURL: "", title: "---", price: 111},
+    {imageURL: "", title: "---", price: 111},
+    // {imageURL: "", title: "---", price: 111},
+    // {imageURL: "", title: "---", price: 111},
+    // {imageURL: "", title: "---", price: 111},
+    // {imageURL: "", title: "---", price: 111},
+  ];
+  // const items: any[] = [];
   const orderPrice = 234532;
   return (
     <div
@@ -34,22 +57,21 @@ const ModalCart = () => {
         isCartOpened ? classes.overlayVisible : ""
       }`}
     >
-      <div className={classes.drawer}>
-        <h2 className={classes.drawerTitle}>
+      <div className={classes.shopCart}>
+        <h2 className={classes.shopCartTitle}>
           Package
           <img
             onClick={onCloseCart}
-            className={classes.drawer__removeBtn}
-            width={32}
-            height={32}
+            className={classes.shopCartCloseBtn}
             src={RemoveIcon}
             alt="Close"
           />
         </h2>
 
         {items && items.length > 0 ? (
-          <div className={classes.drawerItems}>
+          <div className={classes.shopCartItems}>
             <div className={classes.items}>
+              {/* <shopCartItem/> */}
               {items.map(obj => (
                 <div key={obj.imageURL} className={classes.cartItem}>
                   <div
@@ -71,40 +93,19 @@ const ModalCart = () => {
                 </div>
               ))}
             </div>
-            <div className={classes.overlay__total}>
-              <ul>
-                <li className={classes.overlay__total__item}>
-                  <span>Итого:</span>
-                  <b>{orderPrice} руб.</b>
-                </li>
-              </ul>
-              <button
-                // disabled={isLoadingOrders}
-                // onClick={onClickOrder}
-                className={classes.greenBtn}
-              >
-                Оформить заказ <img src={ArrowIcon} alt="Arrow" />
-              </button>
-            </div>
+            <Link
+              to={APP_AUTH_ROUTES.order.link}
+              // onClick={onClickOrder}
+              className={classes.createOrderBtn}
+            >
+              Оформить заказ
+            </Link>
           </div>
         ) : (
           <div className={classes.emptyCart}>
             <img src={EmptyCartImg} alt="emptyCart" width={150} height={150} />
-            Empty
+            Cart empty
           </div>
-          //   <Info
-          //     title={isOrderComlete ? "Order comleted" : "Cart empty"}
-          //     description={
-          //       isOrderComlete
-          //         ? `Your order #${orderId} will sent`
-          //         : "Please add some order"
-          //     }
-          //     image={
-          //       isOrderComlete
-          //         ? "./img/complete-order.jpg"
-          //         : "./img/empty-cart.jpg"
-          //     }
-          //   />
         )}
       </div>
     </div>
